0746682b3ec3087c3044ef043c92047d9dcb5e08
Filip Paryż <paryz.filip@gmail.com>
2020-08-01 22:48:58 +0200

    Left hand working
    Left hand soldered and working, TODO: Right hand and making both work together. Then uploading to QMK.

--- keyboards/handwired/paryzSplit56/config.h
+++ keyboards/handwired/paryzSplit56/config.h
@@ -1,6 +1,4 @@
 /*
-Copyright 2017 Danny Nguyen <danny@hexwire.com>
-
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
@@ -18,3 +16,62 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 #include "config_common.h"
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0x00F5
+#define PRODUCT_ID      0x00A4
+#define DEVICE_VER      0x0100
+#define MANUFACTURER    ParyzFilip
+#define PRODUCT         Split Keyboard
+#define DESCRIPTION     Split handwired ortholinear keyboard
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 10
+#define MATRIX_COLS 6
+
+// wiring of each half
+#define MATRIX_ROW_PINS { C6, D7, E6, B4, B5 }
+#define MATRIX_COL_PINS { B2, B6, F6, B3, B1, F7 }
+#define MASTER_LEFT
+
+/* COL2ROW or ROW2COL */
+#define DIODE_DIRECTION COL2ROW
+
+/* define if matrix has ghost */
+//#define MATRIX_HAS_GHOST
+
+/* Set 0 if debouncing isn't needed */
+#define DEBOUNCE 3
+
+/* serial.c configuration for split keyboard */
+#define SOFT_SERIAL_PIN D0							// TODO Determine the pin to which TRRS is soldered on
+
+/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
+#define LOCKING_SUPPORT_ENABLE
+/* Locking resynchronize hack */
+#define LOCKING_RESYNC_ENABLE
+
+/* ws2812 RGB LED */
+#define RGB_DI_PIN B4
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 10
+#define RGBLED_SPLIT { 5, 5 }
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+// #define NO_DEBUG
+
+/* disable print */
+// #define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION

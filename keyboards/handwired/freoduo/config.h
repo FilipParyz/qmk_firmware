999394a2e45bc77b34a384055159082f1ac41ca0
Filip Paryż <paryz.filip@gmail.com>
2020-10-04 00:05:16 +0200

    Keymap and configuration cleanup

--- keyboards/handwired/freoduo/config.h
+++ keyboards/handwired/freoduo/config.h
@@ -57,21 +57,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 18
 #define RGBLED_SPLIT { 9, 9 }
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-// #define NO_DEBUG
-
-/* disable print */
-// #define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
